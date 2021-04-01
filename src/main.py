cee8c2ccfe6809439c2322b529fe44872f09c93d
Milan Ondrašovič <milan.ondrasovic@gmail.com>
2021-04-01 13:59:20 +0200

    Initialize Python project.

---
+++ src/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Author: Milan Ondrašovič <milan.ondrasovic@gmail.com>
+
+import sys
+
+
+def main():
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
